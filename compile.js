74f9a10ce14695311eb1bd156e56ce9cdf5d4ee5
John Leonardo <jdleonardo5@icloud.com>
2018-07-14 16:30:20 -0700

    add deploy + compile scripts

---
+++ compile.js
@@ -0,0 +1,15 @@
+//bring in path and fs so that we can read .sol file
+const path = require('path');
+const fs = require('fs');
+
+//bring in solidity compiler
+const solc = require('solc');
+
+//generate path to point to lottery.sol contract file
+const lotteryPath = path.resolve(__dirname, 'contracts', 'lottery.sol');
+
+//read .sol source
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+//compile and export contract object
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
